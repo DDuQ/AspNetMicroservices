2b4829c3b9ebe3ce0922f1ba64fb1c5ee72672cc
DDuQ <61994091+DDuQ@users.noreply.github.com>
2022-04-02 22:44:46 -0500

    feat(api): Context, repositories and implementation added

---
+++ src/Services/Catalog/Catalog.API/Extensions/ServiceExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Catalog.API.Data;
+using Catalog.API.Repositories;
+
+namespace Catalog.API.Extensions;
+
+public static class ServiceExtensions
+{
+    public static IServiceCollection AddContext(this IServiceCollection services)
+    {
+        services.AddScoped<ICatalogContext, CatalogContext>();
+        return services;
+    }
+
+    public static IServiceCollection AddRepositories(this IServiceCollection services)
+    {
+        services.AddScoped<IProductRepository, ProductRepository>();
+        return services;
+    }
+}
